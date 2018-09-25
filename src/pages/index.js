6bbf402a4426d3b1fd32b0187f3307e8a2ed5098
Kate Beard <asouthernbellein@gmail.com>
2018-09-25 23:06:53 +0100

    Jiggly letters

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,14 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <div class="grid-container">
-      <div class="j-div purple">J</div>
-      <div class="a1-div purple">A</div>
-      <div class="m-div purple">M</div>
-      <div class="s-div purple">s</div>
-      <div class="t-div purple">t</div>
-      <div class="a2-div purple">a</div>
-      <div class="c-div purple">c</div>
-      <div class="k-div purple">k</div>
+      <div class="j-div purple"><p>J</p></div>
+      <div class="a1-div purple"><p>A</p></div>
+      <div class="m-div purple"><p>M</p></div>
+      <div class="s-div purple"><p>s</p></div>
+      <div class="t-div purple"><p>t</p></div>
+      <div class="a2-div purple"><p>a</p></div>
+      <div class="c-div purple"><p>c</p></div>
+      <div class="k-div purple"><p>k</p></div>
     </div>
   </Layout>
 )
