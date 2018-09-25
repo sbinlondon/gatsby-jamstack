911b96181823853c7a61144c810920a86d8564d1
Kate Beard <asouthernbellein@gmail.com>
2018-09-25 22:56:56 +0100

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <div class="grid-container">
+      <div class="j-div purple">J</div>
+      <div class="a1-div purple">A</div>
+      <div class="m-div purple">M</div>
+      <div class="s-div purple">s</div>
+      <div class="t-div purple">t</div>
+      <div class="a2-div purple">a</div>
+      <div class="c-div purple">c</div>
+      <div class="k-div purple">k</div>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
